2d82ed3852d496e0c90099a2fc42a4c368f77639
Mikolaj Maslanka <mikolaj.mslanka@gmail.com>
2023-03-06 15:24:10 +0100

    executor changed

--- Jenkins/training-jsystems-jenkins/ex4-pipeline.groovy
+++ Jenkins/training-jsystems-jenkins/ex4-pipeline.groovy
@@ -13,20 +13,20 @@ pipeline {
         stage('STAGE-B') {
             parallel {
                 stage('STAGE-C') {
-                    agent { label "agent1" }
+                    agent { label "salve01" }
                     steps {
                         echo "Running STAGE-C..."
                     }
                 }
                 stage('STAGE-D') {
-                    agent { label "agent2" }
+                    agent { label "salve01" }
                     steps {
                         echo "Running STAGE-D..."
                         sh "exit 1"
                     }
                 }
                 stage('STAGE-E') {
-                    agent { label "agent3" }
+                    agent { label "salve01" }
                     steps {
                         echo "Running STAGE-E..."
                     }
